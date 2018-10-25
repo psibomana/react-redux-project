83d75b233210aec31a24b7a0506cfa974efd0e33
Parfait Pascal Sibomana <sasipa90@hotmail.com>
2018-10-25 14:52:51 +0300

    add action creator tests

---
+++ src/__tests__/actions/author.spec.js
@@ -0,0 +1,68 @@
+import {
+  listAuthors,
+  LIST_AUTHORS,
+  deleteAuthor,
+  DELETE_AUTHORS,
+  addAuthor,
+  ADD_AUTHOR,
+  updateAuthor,
+  UPDATE_AUTHORS
+} from "../../actions/author";
+
+describe('Author Actions Test', () => {
+
+  it('should return list authors action object', () => {
+    const authors = [{
+      id: 'firstName-lastName',
+      firstName: 'firstName',
+      lastName: 'lastName'
+    }]
+
+    const listAuthorsAction = listAuthors(authors);
+
+    expect(listAuthorsAction.authors).toEqual(authors);
+    expect(listAuthorsAction.type).toEqual(LIST_AUTHORS);
+  });
+
+  it('should return delete author action object', () => {
+    const index = 'firstName-lastName'
+
+    const deleteAuthorAction = deleteAuthor(index);
+
+    expect(deleteAuthorAction.index).toEqual(index);
+    expect(deleteAuthorAction.type).toEqual(DELETE_AUTHORS);
+  });
+
+  it('should return add author action object', () => {
+    const author = {
+      id: 'firstName-lastName',
+      firstName: 'firstName',
+      lastName: 'lastName'
+    }
+
+    const addAuthorAction = addAuthor(author);
+
+    expect(addAuthorAction.author).toEqual(author);
+    expect(addAuthorAction.author).toHaveProperty("id");
+    expect(addAuthorAction.author).toHaveProperty("firstName");
+    expect(addAuthorAction.author).toHaveProperty("lastName");
+    expect(addAuthorAction.type).toEqual(ADD_AUTHOR);
+  });
+
+  it('should return update author action object', () => {
+    const author = {
+      id: 'firstName-lastName',
+      firstName: 'firstName',
+      lastName: 'lastName'
+    }
+
+    const updateAuthorAction = updateAuthor(author);
+
+    expect(updateAuthorAction.author).toEqual(author);
+    expect(updateAuthorAction.author).toHaveProperty("id");
+    expect(updateAuthorAction.author).toHaveProperty("firstName");
+    expect(updateAuthorAction.author).toHaveProperty("lastName");
+    expect(updateAuthorAction.type).toEqual(UPDATE_AUTHORS);
+  });
+
+});
---
+++ src/__tests__/actions/course.spec.js
@@ -0,0 +1,83 @@
+import {
+  listCourses,
+  LIST_COURSES,
+  deleteCourse,
+  DELETE_COURSE,
+  addCourse,
+  ADD_COURSE,
+  updateCourse,
+  UPDATE_COURSE
+} from "../../actions/course";
+
+describe('Course Actions Test', () => {
+  it('should return list courses action object', () => {
+    const courses = [{
+      id: "web-components-shadow-dom",
+      title: "Web Component Fundamentals",
+      watchHref: "http://www.pluralsight.com/courses/web-components-shadow-dom",
+      authorId: "cory-house",
+      length: "5:10",
+      category: "HTML5"
+    }]
+
+    const listCourseAction = listCourses(courses);
+
+    expect(listCourseAction.courses).toEqual(courses);
+    expect(listCourseAction.type).toEqual(LIST_COURSES);
+  });
+
+  it('should return delete course action object', () => {
+    const index = "web-components-shadow-dom";
+
+    const deleteCourseAction = deleteCourse(index);
+
+    expect(deleteCourseAction.index).toEqual(index);
+    expect(deleteCourseAction.type).toEqual(DELETE_COURSE);
+  });
+
+
+  it('should return add course action object', () => {
+    const course = {
+      id: "web-components-shadow-dom",
+      title: "Web Component Fundamentals",
+      watchHref: "http://www.pluralsight.com/courses/web-components-shadow-dom",
+      authorId: "cory-house",
+      length: "5:10",
+      category: "HTML5"
+    }
+
+    const addCourseAction = addCourse(course);
+
+    expect(addCourseAction.course).toEqual(course);
+    expect(addCourseAction.course).toHaveProperty("id");
+    expect(addCourseAction.course).toHaveProperty("title");
+    expect(addCourseAction.course).toHaveProperty("watchHref");
+    expect(addCourseAction.course).toHaveProperty("authorId");
+    expect(addCourseAction.course).toHaveProperty("length");
+    expect(addCourseAction.course).toHaveProperty("category");
+    expect(addCourseAction.type).toEqual(ADD_COURSE);
+  });
+
+
+  it('should return update course action object', () => {
+    const course = {
+      id: "web-components-shadow-dom",
+      title: "Web Component Fundamentals",
+      watchHref: "http://www.pluralsight.com/courses/web-components-shadow-dom",
+      authorId: "cory-house",
+      length: "5:10",
+      category: "HTML5"
+    }
+
+    const updateCourseAction = updateCourse(course);
+
+    expect(updateCourseAction.course).toEqual(course);
+    expect(updateCourseAction.course).toHaveProperty("id");
+    expect(updateCourseAction.course).toHaveProperty("title");
+    expect(updateCourseAction.course).toHaveProperty("watchHref");
+    expect(updateCourseAction.course).toHaveProperty("authorId");
+    expect(updateCourseAction.course).toHaveProperty("length");
+    expect(updateCourseAction.course).toHaveProperty("category");
+    expect(updateCourseAction.type).toEqual(UPDATE_COURSE);
+  });
+});
