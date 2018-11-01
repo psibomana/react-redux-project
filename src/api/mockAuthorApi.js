5e17ce46a490818e7f3b232ddc16392fe967dec4
Parfait Pascal Sibomana <sasipa90@hotmail.com>
2018-11-01 17:17:28 +0300

    add author api tests

--- src/api/mockAuthorApi.js
+++ src/api/mockAuthorApi.js
@@ -1,8 +1,5 @@
 import delay from './delay';
 
-// This file mocks a web API by working with the hard-coded data below.
-// It uses setTimeout to simulate the delay of an AJAX call.
-// All calls return promises.
 const authors = [
   {
     id: 'cory-house',
